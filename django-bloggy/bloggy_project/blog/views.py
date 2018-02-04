85d9ad5f7e9636f7b726f912390e6d0d16d2551e
qwerty432 <dimichkit@ukr.net>
2018-02-04 20:36:52 +0200

    added counting views of post

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -19,6 +19,8 @@ def index(request):
 def post(request, post_url):
     single_post = get_object_or_404(Post,
     title=post_url.replace('_', ' '))
+    single_post.views += 1  # increment the number of views
+    single_post.save()      # and save it
     t = loader.get_template('blog/post.html')
     c = Context({'single_post': single_post, })
     return HttpResponse(t.render(c))
