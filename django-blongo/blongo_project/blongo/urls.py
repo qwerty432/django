a3de557acef0faca76e3c45bfdf8fe80e464b3fc
qwerty432 <dimichkit@ukr.net>
2018-02-10 00:44:50 +0200

    created app with mongodb

--- django-blongo/blongo_project/blongo/urls.py
+++ django-blongo/blongo_project/blongo/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from blongo import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('blongo.urls')),
+    url(r'^$', views.index, name='index'),
 ]
