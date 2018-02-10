c4a9827bc81c3aa9613c7151c7addbaed4d9aecd
qwerty432 <dimichkit@ukr.net>
2018-02-10 18:12:49 +0200

    added home page

--- django_ecommerce/django_ecommerce/django_ecommerce/urls.py
+++ django_ecommerce/django_ecommerce/django_ecommerce/urls.py
@@ -5,14 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_ecommerce.views.home', name='home'),
-    # url(r'^django_ecommerce/', include('django_ecommerce.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.index', name='home'),
 )
