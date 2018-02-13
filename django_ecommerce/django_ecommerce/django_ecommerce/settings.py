f06a0efec5ac5436496a7e47e9f42ed83e282243
qwerty432 <dimichkit@ukr.net>
2018-02-13 23:10:47 +0200

    started with stripe payment system

--- django_ecommerce/django_ecommerce/django_ecommerce/settings.py
+++ django_ecommerce/django_ecommerce/django_ecommerce/settings.py
@@ -70,11 +70,7 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static'),)
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -121,6 +117,7 @@ INSTALLED_APPS = (
     'main',
     'django.contrib.flatpages',
     'contact',
+    'payments',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
